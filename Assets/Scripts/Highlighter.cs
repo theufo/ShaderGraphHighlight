64e3a710c350ea0e05281dbe3cbea3fa95923e43
Alex Mironov <zettaiingamono@gmail.com>
2020-11-19 19:56:16 +0300

    Initial commit

---
+++ Assets/Scripts/Highlighter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Collider))]
+public class Highlighter : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    [SerializeField]
+    private Material originalMaterial;
+
+    [SerializeField]
+    private Material highlightedMaterial;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        EnableHighlight(false);
+    }
+
+    public void EnableHighlight(bool onOff)
+    {
+        if (meshRenderer != null && originalMaterial != null && highlightedMaterial != null)
+            meshRenderer.material = onOff ? highlightedMaterial : originalMaterial;
+    }
+
+    private void OnMouseOver()
+    {
+        EnableHighlight(true);
+    }
+
+    private void OnMouseExit()
+    {
+        EnableHighlight(false);
+    }
+}
